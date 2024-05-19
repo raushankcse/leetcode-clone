54e4c75779bc618edca5d8ea0829324d937ba413
Raushan Kumar <raushankcse@gmail.com>
2024-05-19 18:30:47 +0530

    utils

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -1,5 +1,6 @@
 import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
 import { useState } from "react";
+import { app } from "../utils/firebase";
 
 
 const actionCodeSettings = {
@@ -13,7 +14,7 @@ const actionCodeSettings = {
 
 export const Signin = () =>{
 
-    const auth = getAuth();
+    const auth = getAuth(app);
     const [email, setEmail] = useState("");
     
     async function onSignin(){
---
+++ frontend/src/utils/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCN3-raoVPQrqCPXaivm2-FzTClqugbmyI",
+  authDomain: "leetcode-clone-5ecf3.firebaseapp.com",
+  projectId: "leetcode-clone-5ecf3",
+  storageBucket: "leetcode-clone-5ecf3.appspot.com",
+  messagingSenderId: "99977825385",
+  appId: "1:99977825385:web:75c515f8fa99cbbed2a6f8",
+  measurementId: "G-3KXT5MY5ES"
+};
+
+export const app = initializeApp(firebaseConfig);
