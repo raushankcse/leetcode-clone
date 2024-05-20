59ed1cceba8f79ced5b6ff62cefff7ae57e84c55
Raushan Kumar <raushankcse@gmail.com>
2024-05-20 13:30:22 +0530

    auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { Landing } from './components/Landing'
 import { Signin } from './components/Signin';
 import {getAuth, onAuthStateChanged } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
+import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
+import { userAtom } from './store/atoms/user';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCN3-raoVPQrqCPXaivm2-FzTClqugbmyI",
@@ -20,20 +22,43 @@ export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app);
 
 function App() {
+  return <RecoilRoot>
+    <StoreApp/>
+  </RecoilRoot>
+}
 
+function StoreApp(){
+  const [user, setUser] = useRecoilState(userAtom);
   useEffect(()=>{
     onAuthStateChanged(auth, function(user){
-      if(user){
-        console.log('This is the user: ', user)
+      if(user && user.email){
+        setUser({
+          loading: false,
+          user: {
+            email: user.email
+          }
+        })
       } else {
+        setUser({
+          loading:false,
+        })
         console.log('There is no logged in user');
       }
     })
   }, [])
 
+ if(user.loading){
+    return <div>loading....</div>
+ }
+  if(!user.user){
+    return <div><Signin/> </div>
+ }
+
+
   return (
+    
     <div>
-      <Signin/>
+      You are logged in as {user.user.email}
     </div>
   )
 }
