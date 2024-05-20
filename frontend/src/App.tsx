5cd5f09f2a649983a040832939bb5db536139ead
Raushan Kumar <raushankcse@gmail.com>
2024-05-20 12:36:00 +0530

    google auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,35 @@
 
+import { useEffect } from 'react';
 import './App.css'
 import { Landing } from './components/Landing'
 import { Signin } from './components/Signin';
+import {getAuth, onAuthStateChanged } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCN3-raoVPQrqCPXaivm2-FzTClqugbmyI",
+  authDomain: "leetcode-clone-5ecf3.firebaseapp.com",
+  projectId: "leetcode-clone-5ecf3",
+  storageBucket: "leetcode-clone-5ecf3.appspot.com",
+  messagingSenderId: "99977825385",
+  appId: "1:99977825385:web:75c515f8fa99cbbed2a6f8",
+  measurementId: "G-3KXT5MY5ES"
+};
 
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app);
 
 function App() {
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, function(user){
+      if(user){
+        console.log('This is the user: ', user)
+      } else {
+        console.log('There is no logged in user');
+      }
+    })
+  }, [])
 
   return (
     <div>
--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -1,7 +1,9 @@
-import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
+import { getAuth, sendSignInLinkToEmail, signInWithPopup } from "firebase/auth";
 import { useState } from "react";
 import { app } from "../utils/firebase";
+import { GoogleAuthProvider } from "firebase/auth";
 
+const provider = new GoogleAuthProvider();
 
 const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
@@ -16,34 +18,41 @@ export const Signin = () =>{
 
     const auth = getAuth(app);
     const [email, setEmail] = useState("");
-    
+     
     async function onSignin(){
-        
-        await sendSignInLinkToEmail(auth, email, actionCodeSettings)
-        .then(() => {
-            // The link was successfully sent. Inform the user.
-            // Save the email locally so you don't need to ask the user for it again
-            // if they open the link on the same device.
-            window.localStorage.setItem('emailForSignIn', email);
-            alert("sent email ")
-            // ... 
-        })
-        .catch((error) => {
-            alert("sent not email")
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ...
-        });
+
+
+            signInWithPopup(auth, provider)
+                .then((result) => {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
+                    if(!credential){
+                        return;
+                    }
+                    const token = credential.accessToken;
+                    // The signed-in user info.
+                    const user = result.user;
+                    console.log(user);
+                    // IdP data available using getAdditionalUserInfo(result)
+                    // ...
+                }).catch((error) => {
+                    // Handle Errors here.
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    // The email of the user's account used.
+                    const email = error.customData.email;
+                    // The AuthCredential type that was used.
+                    const credential = GoogleAuthProvider.credentialFromError(error);
+                    // ...
+                });
     }
 
     return <div>
-        <input type="text" placeholder="email" onChange={(e)=>{
-            setEmail(e.target.value);
-        }} />
+        
         <button onClick={()=>{
             onSignin()
         }}>
-            Signup 
+            Login with google 
         </button>
     </div>
 }
