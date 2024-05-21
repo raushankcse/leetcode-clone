6a3c3947982d2fc97c0fd90edbdb7c92ceb3c820
Raushan Kumar <raushankcse@gmail.com>
2024-05-21 17:38:53 +0530

    firebase added

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,5 +1,5 @@
 import {setGlobalOptions} from "firebase-functions/v2";
-import {onRequest , onCall} from "firebase-functions/v2/https";
+import {onRequest, onCall} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
@@ -19,13 +19,15 @@ export const helloWorld = onRequest((request, response) => {
 });
 
 export const getSubmissions = onRequest(async (request, response) => {
-  
   const limit = request.body.limit || 10;
-  const res = await db.collection("submissions").limit(limit).orderBy("submitTime", "desc").get();
+  const res = await db.collection("submissions")
+    .limit(limit)
+    .orderBy("submitTime", "desc")
+    .get();
   const submissions = [];
-  res.docs.forEach(doc =>{
-    submissions.push(doc.data())
-    console.log(doc.data())
+  res.docs.forEach((doc) =>{
+    submissions.push(doc.data());
+    console.log(doc.data());
   });
 
   response.send({
@@ -39,37 +41,39 @@ export const submit = onCall(async (request)=>{
   const submission = request.data.submission;
   const problemId = request.data.problemId;
 
-  const problem = await db.collection("problems").doc(problemId?.toString()).get();
-  if(!uid){
-    return{
-      message: "Unautorized"
-    }
-  }
-
-   if(!SUPPORTED_LANGUAGES.includes(language)){
-    return{
-      message: "Language not supported"
-    }
-   }
-  
-  if(!problem.exists){
+  const problem = await db.collection("problems")
+    .doc(problemId?.toString())
+    .get();
+  if (!uid) {
     return {
-      message: "Problem Doesn't exist"
-    }
+      message: "Unautorized",
+    };
   }
 
-  const doc =  await db.collection("submission").add({
+  if (!SUPPORTED_LANGUAGES.includes(language)) {
+    return {
+      message: "Language not supported",
+    };
+  }
+
+  if (!problem.exists) {
+    return {
+      message: "Problem Doesn't exist",
+    };
+  }
+
+  const doc = await db.collection("submission").add({
     language,
     submission,
     problemId,
     userId: uid,
     submitTime: new Date(),
     workerTryCount: 0,
-    status: "PENDING"
-  }) 
+    status: "PENDING",
+  });
   return {
     message: "Submission done",
-    id: doc.id
-  }
-})
+    id: doc.id,
+  };
+});
 
