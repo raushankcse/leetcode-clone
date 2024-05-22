f33e48de93df18a11e739c008e255de798c2db78
Raushan Kumar <raushankcse@gmail.com>
2024-05-22 12:04:24 +0530

    backend edit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
 
 import { useEffect } from 'react';
 import './App.css'
-import { Landing } from './components/Landing'
 import { Signin } from './components/Signin';
 import {getAuth, onAuthStateChanged } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
-import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
+// import { initializeApp } from 'firebase/app';
+import { RecoilRoot, useRecoilState } from 'recoil';
 import { userAtom } from './store/atoms/user';
 import { Topbar } from './components/Topbar';
 import { Card } from './components/Card';
+import { app } from './utils/firebase';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCN3-raoVPQrqCPXaivm2-FzTClqugbmyI",
-  authDomain: "leetcode-clone-5ecf3.firebaseapp.com",
-  projectId: "leetcode-clone-5ecf3",
-  storageBucket: "leetcode-clone-5ecf3.appspot.com",
-  messagingSenderId: "99977825385",
-  appId: "1:99977825385:web:75c515f8fa99cbbed2a6f8",
-  measurementId: "G-3KXT5MY5ES"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCN3-raoVPQrqCPXaivm2-FzTClqugbmyI",
+//   authDomain: "leetcode-clone-5ecf3.firebaseapp.com",
+//   projectId: "leetcode-clone-5ecf3",
+//   storageBucket: "leetcode-clone-5ecf3.appspot.com",
+//   messagingSenderId: "99977825385",
+//   appId: "1:99977825385:web:75c515f8fa99cbbed2a6f8",
+//   measurementId: "G-3KXT5MY5ES"
+// };
 
-export const app = initializeApp(firebaseConfig)
+// export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app);
 
 function App() {
